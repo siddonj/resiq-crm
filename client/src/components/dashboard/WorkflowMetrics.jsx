0081651078da7803827de40413af5395065d43e0
siddonj <siddonj@hotmail.com>
2026-04-18 03:42:30 +0000

    approved

--- client/src/components/dashboard/WorkflowMetrics.jsx
+++ client/src/components/dashboard/WorkflowMetrics.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useAuth } from '../context/AuthContext'
+import { useAuth } from '../../context/AuthContext'
 import DashboardCard from './DashboardCard'
 
 export default function WorkflowMetrics() {
